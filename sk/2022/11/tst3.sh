5f43066a24b8b56bff7ae9478ec30982e203e238
BB <me@yomama.com>
2022-10-25 14:33:08 +0300

    -

---
+++ sk/2022/11/tst3.sh
@@ -0,0 +1,2 @@
+python -u tst3.py
+cat /mnt/3d1ece2f-6539-411b-bac2-589d57201626/home/burak/Downloads/C-0.csv /mnt/3d1ece2f-6539-411b-bac2-589d57201626/home/burak/Downloads/C-1.csv /mnt/3d1ece2f-6539-411b-bac2-589d57201626/home/burak/Downloads/C-2.csv /mnt/3d1ece2f-6539-411b-bac2-589d57201626/home/burak/Downloads/C-3.csv > /mnt/3d1ece2f-6539-411b-bac2-589d57201626/home/burak/Downloads/C3.csv

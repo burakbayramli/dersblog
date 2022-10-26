545244d1860033d1243871627fa6acb9feee302d
BB <me@yomama.com>
2022-10-26 12:58:08 +0300

    -

---
+++ sk/2022/11/util.py
@@ -0,0 +1,26 @@
+import os, numpy as np
+
+def process(file_name,ci,N,hookobj):
+    file_size = os.path.getsize(file_name)
+    beg = 0
+    chunks = []
+    hookobj.ci = ci
+    for i in range(N):
+        with open(file_name, 'r') as f:
+            s = int((file_size / N)*(i+1))
+            f.seek(s)
+            f.readline()
+            end_chunk = f.tell()-1
+            chunks.append([beg,end_chunk])
+            f.close()
+        beg = end_chunk+1
+    c = chunks[ci]
+    with open(file_name, 'r') as f:
+        f.seek(c[0])
+        while True:
+            line = f.readline()
+            hookobj.exec(line)
+            if f.tell() > c[1]: break
+        f.close()
+        hookobj.post()
+        

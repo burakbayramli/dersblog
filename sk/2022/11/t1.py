8dca5219fe169c3eb115fc67ab881ba3745f282b
BB <me@yomama.com>
2022-10-28 17:00:01 +0300

    -

---
+++ sk/2022/11/t1.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import os, numpy as np, util
+
+class BucketJob:
+    def __init__(self,B):
+        self.ci = -1
+        self.B = B
+        self.bins = np.array([0, 50000, 80000, 130000, 474391])
+        self.fout = open("/tmp/B-%d-%d.csv" % (self.B,self.ci), "w")
+    def bucket(self, id): # id hangi kutuya ait?
+        return np.argmax(self.bins > id)-1        
+    def exec(self,line):
+        toks = line.strip().split(',')
+        if self.bucket(float(toks[0])) == self.B:
+           self.fout.write(line)
+    def post(self):
+        # diske yaz
+        self.fout.close()
+
+
+class BucketSortJob:
+    def __init__(self,B):
+        self.ci = -1
+        self.res = [] # satirlari burada biriktir
+    def exec(self,line):
+        toks = line.strip().split(',')
+        self.res.append(toks)
+    def post(self):
+        df = pd.DataFrame(self.res) # birikmis satirlarla DataFrame yarat
+        df[0] = pd.to_numeric(df[0])
+        df = df.sort_values(by=0) # hafizada sirala
+        # diske yaz
+        df.to_csv("/tmp/B-%d-sorted.csv" % (self.ci),index=None,header=None)
+        
+# seri isledi ama nihai ortamda paralel isler
+for B in range (4):
+    util.process(file_name='/tmp/input.csv', ci=0, N=4, hookobj = BucketJob(B))
+    util.process(file_name='/tmp/input.csv', ci=1, N=4, hookobj = BucketJob(B))
+    util.process(file_name='/tmp/input.csv', ci=2, N=4, hookobj = BucketJob(B))
+    util.process(file_name='/tmp/input.csv', ci=3, N=4, hookobj = BucketJob(B))
---
+++ sk/2022/11/t1.sh
@@ -0,0 +1,5 @@
+cat /tmp/B-0-*.csv > /tmp/B-0.csv
+cat /tmp/B-1-*.csv > /tmp/B-1.csv
+cat /tmp/B-2-*.csv > /tmp/B-2.csv
+cat /tmp/B-3-*.csv > /tmp/B-3.csv
+

5b7ecd3b29fe6dca67293696f466a262142b2d76
BB <me@yomama.com>
2022-11-14 14:45:23 +0300

    -

--- work/inva/build.py
+++
@@ -1,7 +0,0 @@
-import os, sys
-
-if len(sys.argv) == 1 or sys.argv[1] == 'tex':
-    os.system("pdflatex inva1.tex")
-if len(sys.argv) == 1 or sys.argv[1] == 'zip':
-    os.system("zip -r /home/burak/Downloads/inva.zip PRIMEarxiv.sty inva1.texl ")
-       

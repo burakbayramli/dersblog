0a0bfcb3ac46618e00a12fae3eaddb3f26f5e8d4
BB <me@yomama.com>
2022-12-12 14:13:54 +0300

    -

--- util.py
+++ util.py
@@ -76,6 +76,7 @@ def tex_mathjax_html(texfile, htmlfile, title):
       line = line.replace("\\big\{","\\big\\\\{")
       line = line.replace("\\big\}","\\big\\\\}")
       line = re.sub(r'\\hspace\{(.*?)\}', r'\n', line)
+      line = re.sub(r'\\renewcommand\*\{\\arraystretch\}\{2\.5\}',r'\\renewcommand\*\{\\arraystretch\}\{}',line)
       line = line.replace("\\begin{itemize}","")
       line = line.replace("\\end{itemize}","")
       line = line.replace("\\begin{enumerate}","")

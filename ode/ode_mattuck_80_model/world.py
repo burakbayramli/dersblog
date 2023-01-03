7f3c33a84993b3bf0b18d9aef785bb03b8990c7b
BB <me@yomama.com>
2023-01-03 13:54:46 +0300

    -

--- ode/ode_mattuck_80_model/world.py
+++ ode/ode_mattuck_80_model/world.py
@@ -15,22 +15,21 @@ def resid(p, y, t):
 if __name__ == '__main__':
     t, x1 = np.loadtxt('world.txt', unpack=True)
     t = np.linspace(0,1,len(t))
-    print "len t", len(t)
+    print ("len t", len(t))
     
     A0,k0,K0 = 1, 1, 1
     [A,k,K], flag  = optimize.leastsq(resid, [A0,k0,K0], args=(x1, t))
 
-    print "flag",flag
-    print "A", A
-    print "k",k
-    print "K",K
+    print ("flag",flag)
+    print ("A", A)
+    print ("k",k)
+    print ("K",K)
         
     ahead = 60.
     res = K / (1.+A*np.exp(-k*(1.+(ahead/len(t)))))
-    print res    
+    print (res)
     
     plt.plot(t, x1, 'ro')
-    plt.hold(True)            
     t = np.linspace(0,4,len(t))    
     plt.plot(t, f(t,A,k,K), 'go')    
     

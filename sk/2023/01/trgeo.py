eb69725a90015b3b0ff1ecfd0cebff97d81b1550
BB <me@yomama.com>
2023-01-25 15:24:54 +0300

    -

--- sk/2023/01/trgeo.py
+++ sk/2023/01/trgeo.py
@@ -1,19 +1,27 @@
 import pandas as pd, folium, numpy as np, json
+from pygeodesy.sphericalNvector import LatLon
 
 m = folium.Map(location=[40,33], tiles='Stamen Terrain', zoom_start=7)
+base = "/opt/Downloads/trgeo"
 
+def average(coords):
+    b = (LatLon(lat,lon) for lat,lon in coords)
+    nvecs = np.array([a.toNvector() for a in b])
+    mid = nvecs.mean().toLatLon()
+    return mid.lat,mid.lon
+        
 def camp():
-    df = pd.read_csv('kamp/kampyerleri.csv',sep=';')
+    df = pd.read_csv(base + '/kamp/kampyerleri.csv',sep=';')
     for index, row in df.iterrows():
         lat,lon = (float(x) for x in row['location'].split(","))
         folium.CircleMarker(location=[lon,lat],
                             tooltip=row['description'],
                             radius=6,
-                            color='red',
+                            color='yellow',
                             weight=4).add_to(m)                
         
         
-    df = pd.read_csv('kamp/trkamp.csv',sep=';')
+    df = pd.read_csv(base + '/kamp/trkamp.csv',sep=';')
     for index, row in df.iterrows():
         lat,lon = (float(x) for x in row['location'].split(","))
         folium.CircleMarker(location=[lat,lon],
@@ -23,24 +31,32 @@ def camp():
                             weight=4).add_to(m)                
 
 def park():        
-    files = ['millipark/milli_parklar.json',
-             'millipark/tabiat_anitlari.json',
-             'millipark/ozel_cevre_koruma_alanlari.json',
-             'millipark/tabiati_koruma_alanlari.json',
-             'millipark/tabiat_parklari.json',
-             'millipark/yaban_hayati_gelistirme_sahalari.json',
-             'millipark/sulak_alanlar.json']
+    files = ['/millipark/milli_parklar.json',
+             '/millipark/tabiat_anitlari.json',
+             '/millipark/ozel_cevre_koruma_alanlari.json',
+             '/millipark/tabiati_koruma_alanlari.json',
+             '/millipark/tabiat_parklari.json',
+             '/millipark/yaban_hayati_gelistirme_sahalari.json',
+             '/millipark/sulak_alanlar.json']
 
     for f in files:
         print (f)
-        with open(f, encoding='utf-8') as fh:
+        with open(base + f, encoding='utf-8') as fh:
             data = json.load(fh)
         for name in data.keys():
             data1 = data[name]
             data1 = np.array([[float(x[0]),float(x[1])] for x in data1])
+            if len(data1) < 3: continue
+            clat,clon = average(data1)
+            desc = name + " " + f.replace("/millipark/","").replace(".json","")
+            folium.CircleMarker(location=[clat,clon],
+                                tooltip=desc,
+                                radius=3,
+                                color='green',
+                                weight=4).add_to(m)                
             folium.Polygon(locations=data1,weight=2,color = 'blue').add_to(m)
 
 camp()
 park()
             
-m.save("/tmp/index.html")
+m.save(base + "/trgeo.html")
